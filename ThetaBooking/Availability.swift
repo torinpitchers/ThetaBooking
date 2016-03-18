6ae0965291d5759a03e996c48bbb497dd8b8fda0
Harry Moy <harrymoy@me.com>
2016-03-18 10:20:18 +0000

    Added file

---
+++ ThetaBooking/Availability.swift
@@ -0,0 +1,20 @@
+//
+//  Availability.swift
+//  ThetaBooking
+//
+//  Created by Harry Moy on 18/03/2016.
+//  Copyright © 2016 Harry Moy. All rights reserved.
+//
+
+import Foundation
+
+struct Availability {
+    var name:String
+    var start:String
+    var end:String
+    var date:String
+    var notes:String
+    var reucurring: Bool
+    var bookable: Bool
+    
+}
