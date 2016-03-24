62c27d07068111f4f5135b22a3dca60fefe94e6d
Matthew Copson <Matt@matthews-macbook-pro-2.local>
2016-03-24 13:29:42 +0000

    placed return after code

--- ThetaBooking/AppDelegate.swift
+++ ThetaBooking/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        return true
         application.statusBarStyle = UIStatusBarStyle.LightContent
+        return true
         
     }
 
